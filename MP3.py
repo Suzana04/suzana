94173a8baeccbb712de542ee034ffadf12f699ba
Suzana04 <61520771+Suzana04@users.noreply.github.com>
2020-08-18 21:05:45 -0300

    Add files via upload

---
+++ MP3.py
@@ -0,0 +1,10 @@
+#Feito por Suzana de Santana 2020 como parte dos meus estudos em Python
+#contato: suzana_santana@id.uff.br
+#descrição: exercício do curso em vídeo; programa para tocar uma música em MP3
+
+
+from playsound import playsound
+playsound('Hallelujah.mp3')
+
+
+
